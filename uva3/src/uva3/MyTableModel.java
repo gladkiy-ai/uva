b6c8ce99b2400313ffd8aced17f7ffada634b76c
gladkiy-ai <shmuga666@gmail.com>
2022-01-17 15:05:40 +0300

    Change RU-names un Java-code

--- uva3/src/uva3/MyTableModel.java
+++ uva3/src/uva3/MyTableModel.java
@@ -3,31 +3,31 @@ package uva3;
 import javax.swing.table.AbstractTableModel;
 
 public class MyTableModel extends AbstractTableModel{
-    
+
     public int getRowCount() {
         return GetQuery.outDB[0].length;
     }
-    
+
     public int getColumnCount() {
         return GetQuery.outDB.length;
       }
-    
+
     public Object getValueAt(int r, int c) {
     	return GetQuery.outDB[c][r];
     }
-    
+
     public String getColumnName(int c) {
         switch (c) {
             case 0:
-                return "Сотрудник";
+                return "employee";
             case 1:
-                return "Зарплата";
+                return "salary";
             case 2:
-                return "Департамент";
+                return "department";
             case 3:
-                return "Начальник";
+                return "chief";
             default:
-                return "Зарплата Нач.";
+                return "chief's salary";
                 }
     }
 }
--- uva3/src/uva3/ShowTable.java
+++ uva3/src/uva3/ShowTable.java
@@ -12,46 +12,45 @@ import java.awt.event.ActionListener;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class ShowTable extends JFrame 
+public class ShowTable extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
 	private static XSSFWorkbook wb;
-	
+
 	public ShowTable()  {
-		super("Отображение запроса из БД MySQL");
+		super("View from MySQL DB");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		//Arrays.sort(arrayD, Comparator.comparingInt( GetQuery.outDB ->  GetQuery.outDB[0]));
-		
+
 	    JTable table1 = new JTable(new MyTableModel());
-	    
-	    JButton button1 = new JButton("Фильтр");
+
+	    JButton button1 = new JButton("Filter");
         button1.setActionCommand("Button 1 was pressed!");
-        
-        JButton button2 = new JButton("Сортировка");
+
+        JButton button2 = new JButton("Sort");
         button2.setActionCommand("Button 2 was pressed!");
-        
-        JButton button3 = new JButton("Экспорт в 'out.xlsx' ");
+
+        JButton button3 = new JButton("Export to'out.xlsx' ");
         button3.setActionCommand("Button 3 was pressed!");
-        
+
         button3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
             	JOptionPane.showMessageDialog(rootPane, "JButton Pressed");
             }
         });
-        
+
         Box contents = new Box(BoxLayout.Y_AXIS);
 		contents.add(new JScrollPane(table1));
 		contents.add(button1);
 		contents.add(button2);
 		contents.add(button3);
-		        		
+
 		setContentPane(contents);
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 
-	
-}
 
+}
